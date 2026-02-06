7cc37520507897f0caf6319cf4005c4cade3c5d3
Sangrak Choi <kars@kargn.as>
2026-02-06 13:47:21 +0000

    Update opencode-bar to 2.3.3

--- Casks/opencode-bar.rb
+++ Casks/opencode-bar.rb
@@ -1,6 +1,6 @@
 cask "opencode-bar" do
-  version "2.3.2"
-  sha256 "1c0fd7759fb11cdb50d926b04588becd4c4fdec91f5fb6dd46d272ffb6749685"
+  version "2.3.3"
+  sha256 "08fec4a041eb37263ea41b344b5a0895a976c27d4758794af97a94d04c4b42f1"
 
   url "https://github.com/opgginc/opencode-bar/releases/download/v#{version}/OpenCodeUsageMonitor-v#{version}.dmg"
   name "OpenCode Bar"
